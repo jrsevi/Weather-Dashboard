6a941817d80626bc220a31d7c937c46898d1e85a
jrsevi <jrsevi@gmail.com>
2023-12-13 19:05:01 -0800

    added working forecast

--- assets/script.js
+++ assets/script.js
@@ -9,11 +9,21 @@ function searchByCity(){
     }).then((data) => {
         console.log(data);
         weatherData(data);
+
+        var lat= data.coord.lat;
+        var lon= data.coord.lon;
+        var urlForecast= `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+        fetch(urlForecast).then((response) => {
+            return response.json();
+        }).then((data) => {
+            console.log(data);
+            dayForecast(data);
+        })
     })
     document.getElementById('input').value = "";
     
-
-
+    
+}
 
 
 
@@ -24,13 +34,14 @@ function weatherData(data){
     var tempFahrenheit= Math.round((temp - 273.15) * 9/5 + 32);
     var wind= data.wind.speed;
     var humidity= data.main.humidity;
+    var date= new Date().toDateString(undefined,'Asia/Kolkata');
     
     let icon1= data.weather[0].icon;
     let icon1Url = "http://openweathermap.org/img/wn/" + icon1 + "@2x.png";
     document.getElementById('img').src= icon1Url;
 
 
-    document.getElementById('city').innerHTML = city;
+    document.getElementById('city').innerHTML = city + " " + date;
     document.getElementById('temp').innerHTML = "Temperature: " + tempFahrenheit + "°F";
     document.getElementById('wind').innerHTML = "Windspeed: " + wind;
     document.getElementById('humidity').innerHTML = "Humidity: " + humidity;
@@ -38,31 +49,44 @@ function weatherData(data){
     
 
 }
-}
+
 function dayForecast(forecast){
     document.querySelector('.weekF').innerHTML=''
-    for (let i = 5; i < forecast.list.length; i+=5) {
+    for (let i = 7; i < forecast.list.length; i+=7) {
         console.log(forecast.list[i]);
         let div= document.createElement('div');
         div.setAttribute('class','dayF');
         
         let day= document.createElement('p');
         day.setAttribute('class','date')
-        day.innerText= new Date(forecast.list[i].dt*1000).toDateString(undefined,'Asia/Kolkata');
+        day.innerText= new Date(forecast.list[i].dt * 1000).toDateString(undefined,'Asia/Kolkata');
         div.appendChild(day);
 
         let temp= document.createElement('p');
-        temp.innerText= Math.floor((forecast.list[i].main.temp_max - 273))+ ' °C' + ' / ' + Math.floor((forecast.list[i].main.temp_min - 273))+ ' °C';
+        temp.innerText= Math.floor((forecast.list[i].main.temp_max - 273) * 9/5 + 32)+ ' °F';
         div.appendChild(temp)
 
-        let description= document.createElement('p');
-        description.setAttribute('class','desc')
-        description.innerText= forecast.list[i].weather[0].description;
-        div.appendChild(description);
+        let humidity= document.createElement('p');
+        humidity.innerText= forecast.list[i].main.humidity + '%';
+        div.appendChild(humidity);
+
+        let wind= document.createElement('p');
+        wind.innerText= forecast.list[i].wind.speed + ' mph';
+        div.appendChild(wind);
+
+        let icon= forecast.list[i].weather[0].icon;
+        let iconUrl = "http://openweathermap.org/img/wn/" + icon + ".png";
+        let img= document.createElement('img');
+        img.setAttribute('src',iconUrl);
+        div.appendChild(img);
+
 
         document.querySelector('.weekF').appendChild(div)
     }
 }
+
+
+
 
 
 let searchButton = document.getElementById('searchButton');
